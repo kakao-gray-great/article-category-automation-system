d17963cae8e1ad7b2bf96add26a9109c6c41cda3
oopsys6 <operasystem6@naver.com>
2020-07-31 11:21:27 +0900

    Add app.js

---
+++ app.js
@@ -0,0 +1,106 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+const crawlingByBreakingnews = () => {
+    const category = 'society';
+    // const category = 'politics';
+    let count = 0;
+    for(let day = 1; day <= 20; day++) {
+        for(let page = 1; page <= 5; page++) {
+            // console.log(`https://news.daum.net/breakingnews/${category}?page=${page}&regDate=202006${pad(day, 2)}`);
+            request(`https://news.daum.net/breakingnews/${category}?page=${page}&regDate=202006${pad(day, 2)}`, (error, response, body) => {
+                
+                if(body) {
+                    const $ = cheerio.load(body);
+
+                    let aArr;
+                    aArr = $('a');
+                
+                    let newsArr = [];
+
+                    for(let i = 0; i < aArr.length; i++) {
+                        if(aArr[i].attribs.href.includes("v.daum.net/v/202006") )
+                            newsArr.push(aArr[i].attribs.href);
+                    }
+
+                    newsArr = Array.from(new Set(newsArr));
+
+                    
+                    for(let i = 0; i < newsArr.length; i++) {
+                        console.log(count++, newsArr[i]);
+                        crawlingByNewsByUrl(newsArr[i]);
+                    }
+
+                } else {
+                    return;
+                }       
+            });
+        }
+    }
+}
+
+// 기사 제목 : document.querySelectorAll('.tit_view')[0];
+// 기사 내용 : document.querySelectorAll('#harmonyContainer')[0];
+// 기사 카테고리 : https://api.v.kakao.com/p/346KaedeS2?paths=news,20200521181626807 -> extraInfo.cateInfo.category
+
+const crawlingByNewsByUrl = (url) => {
+    // console.log(url);
+    request(url, (error, response, body) => {
+        if(body) {
+            const $ = cheerio.load(body);
+
+            let title = $('.tit_view')[0].children[0].data;
+            let contentArr = $('#harmonyContainer p');
+            let content = "";
+            for(let i = 0; i < contentArr.length; i++) {
+                if(contentArr[i].children[0] === undefined || contentArr[i].children[0].data === undefined) {
+                    console.log(`[CONTINUE] contentArr[${i}].children[0].data === undefined`);
+                    continue;
+                }
+                content += contentArr[i].children[0].data + " ";
+            }
+
+            let category = $('.gnb_comm')[0].attribs['data-category'];
+
+            let newsObject = {
+                title, 
+                content,
+                category
+            }
+            console.log(newsObject);
+            globalChannel.sendToQueue(queueName, Buffer.from(JSON.stringify(newsObject)));
+
+        } else {
+            return;
+        }
+        
+    });
+}
+
+let globalChannel;
+
+const amqp = require('amqplib/callback_api');
+const queueName = 'PRE_NEWS';
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        channel.assertQueue(queueName, {
+            durable: true
+        });
+        globalChannel = channel;
+
+        crawlingByBreakingnews();
+    });
+});
+
+const pad = (n, width, z) => {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
