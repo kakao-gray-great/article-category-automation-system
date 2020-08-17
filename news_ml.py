929ceeaee3db127a1a5de37aacfaf4849ed52df7
oopsys6 <operasystem6@naver.com>
2020-08-17 19:57:32 +0900

    Use scikit-learn and restore model

--- news_ml.py
+++ news_ml.py
@@ -8,21 +8,38 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
 
 df = pd.read_csv("./news.tsv", delimiter="\t")
-print(df.head(2))
+# print(df.head(2))
 
 df['content'] = df['content'].str.replace('[^ ㄱ-ㅣ가-힣-a-zA-Z]+', '', regex=True)
-print(df.head(100))
+# print(df.head(100))
 
 df = df.fillna(' ')
 
 train_set = df
 X = train_set['content']
-print(X)
+# print(X)
 
 le = LabelEncoder()
-Y = le.fit_transform(train_set['category'])
-print(Y)
+y = le.fit_transform(train_set['category'])
+# print(y)
 
-print(le.classes_)
+# print(le.classes_)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=41, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41, shuffle=True)
+
+clf = Pipeline([('vert', TfidfVectorizer()), ('clf', MultinomialNB())])
+
+model = clf.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+print(classification_report(y_test, pred))
+
+MODEL_NAME = 'model.pkl'
+
+import pickle
+pickle.dump(model, open(MODEL_NAME, 'wb'))
+
+loaded_model = pickle.load(open(MODEL_NAME, 'rb'))
+
+pred = loaded_model.predict(X_test)
+print(classification_report(y_test, pred))
